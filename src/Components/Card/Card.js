30b9aea8699444f9be60ed19ce0f80d879f8be04
mefik1337 <szczechmatt@gmail.com>
2020-02-28 10:57:22 +0100

    changes for style, add variables for scss

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,15 +1,13 @@
 import React from "react";
 import './_Card.scss'
 
-export const Card = ({changeStyles, onDragStart, onDragEnter, item, onDragEnd}) => {
+export const Card = ({changeStyles, onDragStart, onDragEnter, item}) => {
     return (
         <div
             className={changeStyles}
             draggable
             onDragStart={onDragStart}
             onDragEnter={onDragEnter}
-            onDragEnd={onDragEnd}
-
         >
             <p className="Card__text">{item}</p>
         </div>
