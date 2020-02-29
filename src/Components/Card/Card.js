97cf7f8af2f7143bc6ff8bb904b1c4126dcac332
mefik1337 <szczechmatt@gmail.com>
2020-02-29 13:23:48 +0100

    changes for cursor

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './_Card.scss'
 
-export const Card = ({changeStyles, onDragStart, onDragEnter, item}) => {
+export const Card = ({changeStyles, onDragEnd,onDragStart, onDragEnter, item}) => {
     return (
         <div
             className={changeStyles}
             draggable
             onDragStart={onDragStart}
             onDragEnter={onDragEnter}
+            onDragEnd={onDragEnd}
         >
             <p className="Card__text">{item}</p>
         </div>
